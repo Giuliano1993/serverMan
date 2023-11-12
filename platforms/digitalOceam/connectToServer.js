f8406727903b664511075d727b73a33c6ae637af
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-11-12 08:12:59 +0100

    [ add ] connect to ssh interactive

--- platforms/digitalOceam/connectToServer.js
+++ platforms/digitalOceam/connectToServer.js
@@ -1,10 +1,12 @@
 import  inquirer  from "inquirer";
 import { getDroplets } from "./utilities.js";
-import { NodeSSH } from "node-ssh";
+
+import {spawn} from "node:child_process"
+
 
 
 const connectToServer = async ()=>{
-
+   
     const droplets = await getDroplets();
 
     inquirer.prompt([{
@@ -15,23 +17,8 @@ const connectToServer = async ()=>{
     }]).then((answers)=>{
         const droplet = droplets.find(d=>d.name === answers.droplet)
         const {ip_address} = droplet['networks']['v4'].find(ip=>ip.type === "public");
-        console.log(ip_address)
-        const ssh = new NodeSSH();
-        ssh.connect({
-            host: ip_address,
-            username: 'root',
-            privateKeyPath: process.env.localKeyFile
-        }).then(()=>{
-            ssh.execCommand('whoami').then((res)=>{
-                console.log('STDOUT: ' + res.stdout)
-                console.log('STDERR: ' + res.stderr)
-                return ssh.execCommand('exit')
-            }).then(()=>{
-                console.log('Finito')
-                process.exit()
-            })
-        })
-
+        spawn("ssh",['-t',`-i ${process.env.localKeyFile}`, `root@${ip_address}`],{stdio:'inherit'})
+        
 
     })
 
