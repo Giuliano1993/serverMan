494e6093e28191142c27dff097fd051e741006ed
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2024-01-15 22:32:13 +0100

    config if no env

--- serverMan.js
+++ serverMan.js
@@ -1,2 +1,4 @@
 import init from "./init.js";
-init();
+import fs from "fs";
+import {setupConfiguration} from "./utilities/makeConfigs.js";
+!fs.existsSync("./.env") ? setupConfiguration() : init();
